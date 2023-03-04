c0f26346403c2f40f6fc86c161f269052395ea8b
vijaylathiya <lathiyavijay.it2009@gmail.com>
2023-03-04 12:06:31 +0530

    Removed commented admin_bar_menu and related code

--- includes/ot-functions.php
+++ includes/ot-functions.php
@@ -367,31 +367,5 @@ if ( ! function_exists( 'ot_load_google_fonts_css' ) ) {
 
 }
 
-/**
- * Registers the Theme Option page link for the admin bar.
- *
- * @return    void
- *
- * @access    public
- * @since     2.1
- */
-// if ( ! function_exists( 'ot_register_theme_options_admin_bar_menu' ) ) {
-
-//   function ot_register_theme_options_admin_bar_menu( $wp_admin_bar ) {
-    
-//     if ( ! current_user_can( apply_filters( 'ot_theme_options_capability', 'edit_theme_options' ) ) || ! is_admin_bar_showing() )
-//       return;
-    
-//     $wp_admin_bar->add_node( array(
-//       'parent'  => 'appearance',
-//       'id'      => apply_filters( 'ot_theme_options_menu_slug', 'ot-theme-options' ),
-//       'title'   => apply_filters( 'ot_theme_options_page_title', __( 'Theme Options', 'option-tree' ) ),
-//       'href'    => admin_url( apply_filters( 'ot_theme_options_parent_slug', 'themes.php' ) . '?page=' . apply_filters( 'ot_theme_options_menu_slug', 'ot-theme-options' ) )
-//     ) );
-    
-//   }
-  
-// }
-
 /* End of file ot-functions.php */
 /* Location: ./includes/ot-functions.php */
